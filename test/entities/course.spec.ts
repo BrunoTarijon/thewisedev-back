c04a92752be5445e9a0eae037a42e08f8afa33ca
Bruno Aumiller Tarijon <btarijon@gmail.com>
2022-01-16 02:50:26 -0300

    refa: refactoring course test

--- test/entities/course.spec.ts
+++ test/entities/course.spec.ts
@@ -1,137 +1,134 @@
 import { Course, Module, Lecture } from '../../src/entities'
 
+function createCourse(){
+  const course = new Course(
+    'azure-devops',
+    'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git',
+    )
+  return course
+}
+
+function createModule(){
+  const module = new Module(
+    'Fundamentals',
+  )
+  return module
+}
+
+function createLecture(){
+  const lecture: Lecture = new Lecture(
+    'Branching', 'https://youtube.com/1234',
+  )
+  return lecture
+}
+
 describe('Course', () => {
-  it('should be able to add modules to courses', () => {
-    const course = new Course('azure-devops',
-      'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
-    const module = new Module('Fundamentals')
-    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    module.add(lecture)
-    course.add(module)
-    expect(course.includes(module)).toBeTruthy()
-  })
+  describe('Adding courses', () =>{
+    let course: Course
+    let module1: Module
+    let module2: Module
+    let lecture: Lecture
 
-  it('should not be able to add modules with same name', () => {
-    const course = new Course('azure-devops',
-      'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
-    const module1 = new Module('Fundamentals')
-    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+    beforeEach(() =>{
+      course = createCourse()
+      module1 = createModule()
+      module2 = createModule()
+      lecture = createLecture()
+    })
 
-    const module2 = new Module('Fundamentals')
-    module1.add(lecture)
-    course.add(module1)
-    course.add(module2)
-    expect(course.includes(module1)).toBeTruthy()
-    expect(course.includes(module2)).toBeFalsy()
-    expect(course.numberOfModules).toEqual(1)
-  })
-
-  it('should be able to rearrange the order of lectures', () => {
-    const course = new Course('azure-devops',
-      'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
-
-    const fundamentalsModule = new Module('Fundamentals')
-    const branchingLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    fundamentalsModule.add(branchingLecture)
-
-    const courseOverviewModule = new Module('Course Overview')
-    const courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/1934')
-    courseOverviewModule.add(courseOverviewLecture)
-
-    const gitModule = new Module('Source Control with git on Azure DevOps')
-    const introductionLecture = new Lecture('Introduction', 'https://youtube.com/1123')
-    gitModule.add(introductionLecture)
-
-    course.add(fundamentalsModule)
-    course.add(courseOverviewModule)
-    course.add(gitModule)
-
-    course.move(courseOverviewModule, 1)
-
-    expect(course.position(courseOverviewModule)).toBe(1)
-    expect(course.position(fundamentalsModule)).toBe(2)
-    expect(course.position(gitModule)).toBe(3)
-  })
-
-  it('should handle exceeding position while rearranging', () => {
-    const course = new Course('azure-devops',
-      'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
-
-    const fundamentalsModule = new Module('Fundamentals')
-    const branchingLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    fundamentalsModule.add(branchingLecture)
-
-    const courseOverviewModule = new Module('Course Overview')
-    const courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/1934')
-    courseOverviewModule.add(courseOverviewLecture)
-
-    const gitModule = new Module('Source Control with git on Azure DevOps')
-    const introductionLecture = new Lecture('Introduction', 'https://youtube.com/1123')
-    gitModule.add(introductionLecture)
-
-    course.add(fundamentalsModule)
-    course.add(courseOverviewModule)
-    course.add(gitModule)
-
-    course.move(fundamentalsModule, 10)
-
-    expect(course.position(fundamentalsModule)).toBe(1)
-    expect(course.position(courseOverviewModule)).toBe(2)
-    expect(course.position(gitModule)).toBe(3)
+    it('should be able to add modules to courses', () => {
+      module1.add(lecture)
+      course.add(module1)
+      expect(course.includes(module1)).toBeTruthy()
+    })
+  
+    it('should not be able to add modules with same name', () => {
+      module1.add(lecture)
+      course.add(module1)
+      course.add(module2)
+      expect(course.includes(module1)).toBeTruthy()
+      expect(course.includes(module2)).toBeFalsy()
+      expect(course.numberOfModules).toEqual(1)
+    })
   })
   
-  it('should handle negative position while rearranging', () => {
-    const course = new Course('azure-devops',
-      'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
+  describe('Rearrange modules', () => {
+    let course: Course
+    let fundamentalsModule: Module
+    let branchingLecture: Lecture
+    let courseOverviewModule: Module
+    let courseOverviewLecture: Lecture
+    let gitModule: Module
+    let introductionLecture: Lecture
 
-    const fundamentalsModule = new Module('Fundamentals')
-    const branchingLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    fundamentalsModule.add(branchingLecture)
+    beforeEach(() => {
+      course = createCourse()
+      
+      fundamentalsModule = new Module('Fundamentals')
+      branchingLecture = new Lecture('Branching', 'https://youtube.com/1234')
+      fundamentalsModule.add(branchingLecture)
 
-    const courseOverviewModule = new Module('Course Overview')
-    const courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/1934')
-    courseOverviewModule.add(courseOverviewLecture)
+      courseOverviewModule = new Module('Course Overview')
+      courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/1934')
+      courseOverviewModule.add(courseOverviewLecture)
 
-    const gitModule = new Module('Source Control with git on Azure DevOps')
-    const introductionLecture = new Lecture('Introduction', 'https://youtube.com/1123')
-    gitModule.add(introductionLecture)
+      gitModule = new Module('Source Control with git on Azure DevOps')
+      introductionLecture = new Lecture('Introduction', 'https://youtube.com/1123')
+      gitModule.add(introductionLecture)
 
-    course.add(fundamentalsModule)
-    course.add(courseOverviewModule)
-    course.add(gitModule)
+      course.add(fundamentalsModule)
+      course.add(courseOverviewModule)
+      course.add(gitModule)
+    })
 
-    course.move(fundamentalsModule, -1)
-
-    expect(course.position(fundamentalsModule)).toBe(1)
-    expect(course.position(courseOverviewModule)).toBe(2)
-    expect(course.position(gitModule)).toBe(3)
+    it('should be able to rearrange the order of modules', () => {
+      course.move(courseOverviewModule, 1)
+      expect(course.position(courseOverviewModule)).toBe(1)
+      expect(course.position(fundamentalsModule)).toBe(2)
+      expect(course.position(gitModule)).toBe(3)
+    })
+  
+    it('should handle exceeding position while rearranging', () => {
+      course.move(fundamentalsModule, 10)
+      expect(course.position(fundamentalsModule)).toBe(1)
+      expect(course.position(courseOverviewModule)).toBe(2)
+      expect(course.position(gitModule)).toBe(3)
+    })
+    
+    it('should handle negative position while rearranging', () => {
+      course.move(fundamentalsModule, -1)
+      expect(course.position(fundamentalsModule)).toBe(1)
+      expect(course.position(courseOverviewModule)).toBe(2)
+      expect(course.position(gitModule)).toBe(3)
+    })
   })
+  
+  describe('Move lecture between modules',() => {
+    let course: Course
+    let fundamentalsModule: Module
+    let branchingLecture: Lecture
+    let gitModule: Module
+    let introductionLecture: Lecture
 
-  it('should be able to move a lecture to a different module', () => {
-    const course = new Course('azure-devops',
-      'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
+    beforeEach(() => {
+      course = createCourse()
 
-    const fundamentalsModule = new Module('Fundamentals')
-    const branchingLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    fundamentalsModule.add(branchingLecture)
+      fundamentalsModule = new Module('Fundamentals')
+      branchingLecture= new Lecture('Branching', 'https://youtube.com/1234')
+      fundamentalsModule.add(branchingLecture)
 
-    const courseOverviewModule = new Module('Course Overview')
-    const courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/1934')
-    courseOverviewModule.add(courseOverviewLecture)
+      gitModule = new Module('Source Control with git on Azure DevOps')
+      introductionLecture = new Lecture('Introduction', 'https://youtube.com/1123')
+      gitModule.add(introductionLecture)
 
-    const gitModule = new Module('Source Control with git on Azure DevOps')
-    const introductionLecture = new Lecture('Introduction', 'https://youtube.com/1123')
-    const lecture2 = new Lecture('Lecture 2', 'https://youtube.com/1123')
-    gitModule.add(introductionLecture)
-    gitModule.add(lecture2)
-
-    course.add(fundamentalsModule)
-    course.add(courseOverviewModule)
-    course.add(gitModule)
-
-    course.moveLecture(branchingLecture, fundamentalsModule, gitModule, 2)
-    expect(fundamentalsModule.numberOfLectures).toEqual(0)
-    expect(gitModule.numberOfLectures).toEqual(3)
-    expect(gitModule.position(branchingLecture)).toEqual(2)
+      course.add(fundamentalsModule)
+      course.add(gitModule)
+    })
+    it('should be able to move a lecture to a different module', () => {
+      course.moveLecture(branchingLecture, fundamentalsModule, gitModule, 1)
+      expect(fundamentalsModule.numberOfLectures).toEqual(0)
+      expect(gitModule.numberOfLectures).toEqual(2)
+      expect(gitModule.position(branchingLecture)).toEqual(1)
+    })
   })
 })
